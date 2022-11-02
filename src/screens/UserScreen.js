7ddbfd30509e7179f5e375dc8488ea90454197f7
Noor Ahmed <nq73399@gmail.com>
2022-11-02 17:22:02 +0500

    Red Colored Text Render

--- src/screens/UserScreen.js
+++ src/screens/UserScreen.js
@@ -32,15 +32,22 @@ export default function UserScreen() {
   }
 
 
+  function redColored(value) {
+    return (
+      <Text style={{ ...styles.text,fontFamily: fonts.Regular,color: Colors.red}}>{value}</Text>
+    );
+  }
+  
+
   const searchResults = () => {
     var searchItemIndex = jsonData.findIndex(e => e.name === search);
     if(searchItemIndex == -1){
       setTableData([]);
       return Alert.alert('Error', 'This user name does not exist!');
     }
-    let filter = jsonData.slice(0,searchItemIndex > 9 ? 9 : 10).map((val,ind)=>[val?.name, ind + 1, val?.bananas, searchItemIndex == ind ? 'yes' : 'no']);
+    let filter = jsonData.slice(0,searchItemIndex > 9 ? 9 : 10).map((val,ind)=>[searchItemIndex == ind ? redColored(val?.name) : val?.name, ind + 1, val?.bananas, searchItemIndex == ind ? 'yes' : 'no']);
     if(searchItemIndex > 9){
-      filter.push([jsonData[searchItemIndex].name, searchItemIndex + 1, jsonData[searchItemIndex].bananas, 'yes']);
+      filter.push([redColored(jsonData[searchItemIndex].name), searchItemIndex + 1, jsonData[searchItemIndex].bananas, 'yes']);
     }
     
     console.log("filter", filter);
@@ -49,81 +56,93 @@ export default function UserScreen() {
 
   return (
     <View style={styles.container}>
+      <ScrollView style={{marginVertical: Metrix.VerticalSize(20)}}>
+        <View style={styles.imageContainer}>
+          <Image
+            source={Images.logo}
+            resizeMode={'contain'}
+            style={styles.image}
+          />
+        </View>
 
-        <ScrollView style={{marginVertical: Metrix.VerticalSize(20)}}>
-            <View style={styles.imageContainer}>
-                <Image
-                    source={Images.logo}
-                    resizeMode={'contain'}
-                    style={styles.image}
-                />
+        <View style={styles.searchContainer}>
+          <View style={{width: '67%'}}>
+            <View style={styles.eyeIconStyle}>
+              <Ionicons
+                name={'search'}
+                color={Colors.secondary}
+                size={Metrix.customFontSize(20)}
+              />
             </View>
+            <TextField
+              value={search}
+              style={{paddingLeft: Metrix.HorizontalSize(40)}}
+              onChangeText={text => setSearch(text)}
+              placeholder={'Search by User'}
+            />
+          </View>
+          <View style={{width: '28%', marginTop: Metrix.VerticalSize(8)}}>
+            <Button buttonText={'Search'} shadow onPress={searchResults} />
+          </View>
+        </View>
 
-            <View style={styles.searchContainer}>
-              <View style={{width: '67%'}}>
-                  <View style={styles.eyeIconStyle}>
-                    <Ionicons
-                      name={'search'}
-                      color={Colors.secondary}
-                      size={Metrix.customFontSize(20)}
-                    />
-                  </View>
-                  <TextField
-                    value={search}
-                    style={{ paddingLeft: Metrix.HorizontalSize(40) }}
-                    onChangeText={text => setSearch(text)}
-                    placeholder={'Search by User'}
-                  />
-              </View>
-                <View style={{ width: '28%', marginTop: Metrix.VerticalSize(8) }}>
-                    <Button
-                      buttonText={'Search'}
-                      shadow
-                      onPress={searchResults}
-                    />
-                </View>
-              </View>
-
-              <View style={{marginVertical: Metrix.VerticalSize(10)}}>
-                <Table borderStyle={{borderWidth: 2, borderColor: Colors.primary}}>
-                  <Row data={tableHead} style={styles.head} textStyle={styles.text}/>
-                  <Rows data={tableData} textStyle={{...styles.text, fontFamily: fonts.Regular, color: Colors.black}} />
-                </Table>
-              </View>
-            
-        </ScrollView>
+        <View style={{marginVertical: Metrix.VerticalSize(10)}}>
+          <Table borderStyle={{borderWidth: 2, borderColor: Colors.primary}}>
+            <Row data={tableHead} style={styles.head} textStyle={styles.text} />
+            <Rows
+              data={tableData}
+              textStyle={{
+                ...styles.text,
+                fontFamily: fonts.Regular,
+                color: Colors.black,
+              }}
+            />
+          </Table>
+        </View>
+      </ScrollView>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1, 
+  container: {
+    flex: 1,
 
-        paddingHorizontal: Metrix.HorizontalSize(20),
-        backgroundColor: Colors.white
-    },
-    imageContainer: {
-        alignSelf: 'center',
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginTop: Metrix.VerticalSize(20),
-        width: Metrix.VerticalSize(80),
-        height: Metrix.VerticalSize(100),
-      },
-      image: {
-        height: '100%',
-        width: '100%',
-      },
-      searchContainer: { marginBottom: Metrix.VerticalSize(10), flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center' },
-      eyeIconStyle: {
-        position: 'absolute',
-        zIndex: 100,
-        top: Metrix.VerticalSize(25),
-        left: Metrix.HorizontalSize(15),
-      },
+    paddingHorizontal: Metrix.HorizontalSize(20),
+    backgroundColor: Colors.white,
+  },
+  imageContainer: {
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: Metrix.VerticalSize(20),
+    width: Metrix.VerticalSize(80),
+    height: Metrix.VerticalSize(100),
+  },
+  image: {
+    height: '100%',
+    width: '100%',
+  },
+  searchContainer: {
+    marginBottom: Metrix.VerticalSize(10),
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  eyeIconStyle: {
+    position: 'absolute',
+    zIndex: 100,
+    top: Metrix.VerticalSize(25),
+    left: Metrix.HorizontalSize(15),
+  },
 
-      head: {  backgroundColor: Colors.secondary },
-      text: { marginHorizontal: Metrix.HorizontalSize(14),marginVertical: Metrix.VerticalSize(6), color: Colors.white, fontSize: Metrix.customFontSize(11), fontFamily: fonts.Bold, textAlign: 'center' }
-
-})
+  head: {backgroundColor: Colors.secondary},
+  text: {
+    marginHorizontal: Metrix.HorizontalSize(14),
+    marginVertical: Metrix.VerticalSize(6),
+    color: Colors.white,
+    fontSize: Metrix.customFontSize(11),
+    fontFamily: fonts.Bold,
+    textAlign: 'center',
+  },
+});
