bbecde9a0995dc146d5b2c184ecbf6251b430722
Noor Ahmed <nq73399@gmail.com>
2022-11-02 15:04:55 +0500

    Updated Logically completed

--- src/screens/UserScreen.js
+++ src/screens/UserScreen.js
@@ -1,15 +1,51 @@
-import { Image, ScrollView, StyleSheet, Text, View } from 'react-native'
-import React, { useState } from 'react'
+import { Alert, Image, ScrollView, StyleSheet, Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import { Colors, Images, Metrix } from '../config'
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { Button, TextField } from '../components';
 import { fonts } from '../config/Constants';
 import { Table, Row, Rows } from 'react-native-table-component';
+let json = require('../config/leaderboard.json');
 
 export default function UserScreen() {
   const [search,setSearch] = useState('');
+  const [jsonData,setJsonData] = useState(null);
   const tableHead = ['Name', 'Rank', 'Bananas', 'Searched User?'];
   const [tableData,setTableData] = useState([]);
+
+
+
+  useEffect(()=>{   
+    fetchJsonData();
+  },[]);
+
+  const fetchJsonData = () => {
+    let res = [];
+    for(var i in json){
+      res.push(json[i]);
+    }
+    res = res.sort(function (a, b) {
+      return b.bananas - a.bananas;
+    });
+    console.log("resss=======>", res);
+    setJsonData(res);
+  }
+
+
+  const searchResults = () => {
+    var searchItemIndex = jsonData.findIndex(e => e.name === search);
+    if(searchItemIndex == -1){
+      return Alert.alert('Error', 'This user name does not exist!');
+    }
+    let filter = jsonData.slice(0,searchItemIndex > 9 ? 9 : 10).map((val,ind)=>[val?.name, ind + 1, val?.bananas, searchItemIndex == ind ? 'yes' : 'no']);
+    if(searchItemIndex > 9){
+      filter.push([jsonData[searchItemIndex].name, searchItemIndex + 1, jsonData[searchItemIndex].bananas, 'yes']);
+    }
+    
+    console.log("filter", filter);
+    setTableData(filter);
+  }
+
   return (
     <View style={styles.container}>
 
@@ -35,13 +71,14 @@ export default function UserScreen() {
                     value={search}
                     style={{ paddingLeft: Metrix.HorizontalSize(40) }}
                     onChangeText={text => setSearch(text)}
-                    placeholder={'Search by booking ID'}
+                    placeholder={'Search by User'}
                   />
               </View>
                 <View style={{ width: '28%', marginTop: Metrix.VerticalSize(8) }}>
                     <Button
                       buttonText={'Search'}
                       shadow
+                      onPress={searchResults}
                     />
                 </View>
               </View>
@@ -49,7 +86,7 @@ export default function UserScreen() {
               <View style={{marginVertical: Metrix.VerticalSize(10)}}>
                 <Table borderStyle={{borderWidth: 2, borderColor: Colors.primary}}>
                   <Row data={tableHead} style={styles.head} textStyle={styles.text}/>
-                  <Rows data={tableData} textStyle={styles.text} />
+                  <Rows data={tableData} textStyle={{...styles.text, color: Colors.black}} />
                 </Table>
               </View>
             
